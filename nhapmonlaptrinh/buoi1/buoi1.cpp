93df16eb45a6a14f7410a9e5e95a7fdc16a4078a
Administrator <Administrator@PMT2006>
2025-11-19 14:45:27 +0700

    thuc hien xong buoi 1

--- nhapmonlaptrinh/buoi1/buoi1.cpp
+++ nhapmonlaptrinh/buoi1/buoi1.cpp
@@ -6,7 +6,7 @@
 int main()
 {
     std::cout << "Hello World!\n";
-}
+}// 3 bo dong gia 100k
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
