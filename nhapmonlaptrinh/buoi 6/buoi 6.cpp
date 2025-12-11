fbcd44c06b93fcd4c9c79247d62cce420b694850
nmlt <Administrator@PMT2006>
2025-12-11 14:01:07 +0700

    bai tap 3 buoi 6

--- nhapmonlaptrinh/buoi 6/buoi 6.cpp
+++ nhapmonlaptrinh/buoi 6/buoi 6.cpp
@@ -10,8 +10,27 @@ void cau1() {
 	}
 	cout << "tong: " << tong << endl;
 }
+
+void cau2() {
+	int n, tich = 1;
+	cout << "nhap n: ";
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		tich *= i;
+	}
+	cout << "tich: " << tich << endl;
+}
+void cau3() {
+	int n,x, tich = 1;
+	cout << "nhap x, n: ";
+	cin >> x >> n;
+	for (int i = 1; i <= n; i++) {
+		tich *= x;
+	}
+	cout << "tich: " << tich << endl;
+}
 int main() {
-	cau1();
+	cau3();
 }
 
    
