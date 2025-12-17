71bdd54667976a6675d28f88f6d6d0465f5df257
Administrator <Administrator@PMT2006>
2025-12-17 15:04:18 +0700

    bai tap 6 buoi 7

--- nhapmonlaptrinh/buoi7/buoi7.cpp
+++ nhapmonlaptrinh/buoi7/buoi7.cpp
@@ -1,10 +1,5 @@
-#include <iostream>
-#include "songuyen.h"
-using namespace std;
+#include "baitap6.h"
 
-int main()
-{
-	int a = 25, b = 60;
-	int uocchunglonnhat = UCLN(a, b);
-	cout << " uoc chung lon nhat: " << uocchunglonnhat << endl;
+int main() {
+	cau11();
 }
